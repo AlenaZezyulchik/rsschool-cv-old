c98533319fbe4221ac628d5556f1748a76a06fff
Aliaksandr Ziaziulchyk <aliaksandr_ziaziulchyk@epam.com>
2022-07-09 11:52:05 +0300

    feat: added adaptive layot

--- travel/script.js
+++ travel/script.js
@@ -1,1 +1,39 @@
-console.log("Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20\n110 баллов")
+
+let menuWrapper = document.querySelector('.menu_wrapper');
+let menuAdaptiveBurger = document.querySelector('.menu_adaptive_burger');
+let menuAdaptiveClose = document.querySelector('.menu_adaptive_close');
+let burgerMenuOverlay = document.querySelector('.burger_menu_overlay')
+let menuHeaderItem = document.querySelectorAll('.menu_header_item');
+let menuHeaderAdaptive = document.querySelectorAll('menu_header_adaptive');
+
+menuAdaptiveBurger.addEventListener('click', function(){
+	menuWrapper.classList.add('active');
+	burgerMenuOverlay.classList.add('active');
+	menuAdaptiveBurger.style.display='none';
+})
+
+menuAdaptiveClose.addEventListener('click', function(){
+	menuWrapper.classList.remove('active');
+	burgerMenuOverlay.classList.remove('active');
+	menuAdaptiveBurger.style.display='block';
+})
+
+burgerMenuOverlay.addEventListener('click', function(){
+	menuWrapper.classList.remove('active');
+	burgerMenuOverlay.classList.remove('active');
+	menuAdaptiveBurger.style.display='block';
+})
+
+menuHeaderItem.forEach(item => {
+	item.addEventListener('click', () => {
+		menuWrapper.classList.remove('active');
+		menuAdaptiveBurger.style.display='block';
+	})
+})
+
+menuHeaderAdaptive.forEach(item => {
+	item.addEventListener('click', () => {
+		menuWrapper.classList.remove('active');
+		menuAdaptiveBurger.style.display='block';
+	})
+})
